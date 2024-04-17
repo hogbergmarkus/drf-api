44123567ff638588372961bd0b0b26743ad2c3cb
Markus <144561346+Xeptit@users.noreply.github.com>
2024-04-17 13:31:56 +0200

    Some configuration

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL =
-    "https://first-rest-framework-api-f72d1788f2ab.herokuapp.com/";
+// axios.defaults.baseURL = "https://first-rest-framework-api-f72d1788f2ab.herokuapp.com/";
 
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
